57d515e8766e17bf0f264fc20528c64c3c56c2bb
Lucas <lucas.nune@outlook.com.br>
2021-11-19 17:03:05 -0300

    Estudo sobre entrada de dados completo

--- data_input/src/Main.java
+++ data_input/src/Main.java
@@ -2,19 +2,27 @@ import java.util.Scanner;
 
 public class Main {
 	
-	
-	
 	public static void main(String[] args) {
 		
 		
-		entradaString();
-		System.out.println();
-		entradaInteiro();
-		System.out.println();
-		entradaDouble();
+		//entradaString();
+		//System.out.println();
 		
+		//entradaInteiro();
+		//System.out.println();
+		
+		//entradaDouble();
+		//System.out.println();
+		
+		//entradaChar();
+		//System.out.println();
+		
+		//variasEntradas();
+		
+		 entradaLinhas();
 	}
-		
+	
+	//Entrada de Letras|Palavras
 	public static void entradaString() {
 		
 		
@@ -28,6 +36,7 @@ public class Main {
 	
 	}
 	
+	//Entrada de Inteiro
 	public static void entradaInteiro() {
 		
 		Scanner sc = new Scanner(System.in);
@@ -40,6 +49,7 @@ public class Main {
 		
 	}
 	
+	//Entrada de Double
 	public static void entradaDouble() {
 		Scanner sc = new Scanner(System.in);
 		double x;
@@ -48,4 +58,63 @@ public class Main {
 		x = sc.nextDouble();
 		System.out.println("Você digitou: " + x);
 	}
+	
+	//Entrada de Char
+	public static void entradaChar() {
+		
+		Scanner sc = new Scanner(System.in);
+		char x; 
+		
+		System.out.print("Digite um caractere: ");
+		x = sc.next().charAt(0);
+		System.out.println("Você digitou:" + x );
+	}
+	
+	//Exemplo Varias entradas de Teste
+	public static void variasEntradas() {
+		
+		Scanner sc = new Scanner(System.in);
+		int x; 
+		String y; 
+		char z;
+		double c;
+		
+		System.out.print("Digite um número Inteiro: ");
+		x = sc.nextInt();
+		
+		System.out.print("Digite um número flutuante: ");
+		c = sc.nextDouble();
+		
+		System.out.print("Digite uma Palavra: ");
+		y = sc.next();
+		
+		System.out.print("Digite um caractere:");
+		z = sc.next().charAt(0);
+		
+		System.out.printf("O valores colocados nas variaveis são x: %d - c: %.2f - y: %s -  z: %s", x,c,y,z);
+		
+	}
+	
+	public static void entradaLinhas() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x;
+		String s1, s2, s3;
+		
+		x = sc.nextInt();
+		sc.nextLine(); //Limpando Buffer
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
+		
+		System.out.println("Dados Digitados: ");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		
+		sc.close();
+				
+	}
+	
 }
