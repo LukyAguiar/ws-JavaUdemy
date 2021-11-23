e451d1da2f73bcd99724decbff61c2e33db6a444
Lucas <lucas.nune@outlook.com.br>
2021-11-23 12:35:42 -0300

    Func Math

---
+++ math/src/math/Main.java
@@ -0,0 +1,72 @@
+package math;
+
+public class Main {
+	
+	
+	public static void main(String[] args){		
+		
+		raizquadrada();
+		potencia();
+		absoluto();
+		bhaskara();
+	}
+	
+	
+	public static void raizquadrada(){
+		
+		double x = 3.0;
+		double y = 4.0; 
+		double A, B, C;
+		
+		A = Math.sqrt(x);
+		B = Math.sqrt(y);
+		C = Math.sqrt(25.0);
+		System.out.println("Raiz quadrada de " + x + " = " + A);
+		System.out.println("Raiz quadrada de " + y + " = " + B);
+		System.out.println("Raiz quadrada de 25" + " = " + C);
+		System.out.println();		
+	}
+	
+	public static void potencia() {
+		
+		double x = 3.0;
+		double y = 4.0; 
+		double A, B, C;
+		
+		A = Math.pow(x, y);
+		B = Math.pow(x, 2.0);
+		C = Math.pow(5.0, 2.0);
+		System.out.println(x + " elevado a " + y + " = " + A);
+		System.out.println(x + " elevado ao quadrado = " + B);
+		System.out.println("5 elevado ao quadrado = " + C);
+		System.out.println();
+		
+	}
+	
+	public static void absoluto() {
+
+		double y = 4.0; 
+		double z = -5.0;
+		double A, B;
+		
+		A = Math.abs(y); 
+		B = Math.abs(z);
+		System.out.println("Valor absoluto de " + y + " = " + A);
+		System.out.println("Valor absoluto de " + z + " = " + B);
+	}
+	
+	public static void bhaskara() {
+		 double delta; 
+		 double x1,x2 ;
+		 double a = 5, b = 6, c = 4;
+		 
+		 delta = Math.pow(b, 2.0) - 4* a * c ;
+		 
+		 x1 = (-b + Math.sqrt(delta)) / (2.0 * a);
+		 x2 = (-b - Math.sqrt(delta)) / (2.0 * a);
+		 
+		 System.out.println("X1 = " + x1);
+		 System.out.println("X2 = " + x2);
+		 System.out.println("Valor de Delta é " + delta);
+	}
+}
